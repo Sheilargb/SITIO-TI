4bcd34d9f56e1d4069406fcc13a29b336d7d4d3c
Sheila Yuridia Cruz Badillo <240314@utxicotepec.edu.mx>
2026-01-08 14:29:01 -0600

    Sitio TI:
    create: se crearon funciones JS y estilo del encabezado

--- JS/funciones.js
+++ JS/funciones.js
@@ -1,3 +1,27 @@
 function saludar(){
-    alert("Buenvenido a la Academia de T.I")
+    alert("Buenvenido a la Academia de T.I");
+}
+function suma() {
+    var valor1= 24;
+    var valor2= 13;
+    var resultado= valor1 + valor2;
+    alert("El resultado de la suma de "+valor1+" y "+valor2+" es "+resultado);
+}
+function resta() {
+    var valor1= 24;
+    var valor2= 13;
+    var resultado= valor1 - valor2;
+    alert("El resultado de la resta de "+valor1+" menos "+valor2+" es "+resultado);
+}
+function multiplicacion() {
+    var valor1= 5;
+    var valor2= 6;
+    var resultado= valor1 * valor2;
+    alert("El resultado de la multiplicacion de "+valor1+" y "+valor2+" es "+resultado);
+}
+function division() {
+    var valor1= 48;
+    var valor2= 6;
+    var resultado= valor1 / valor2;
+    alert("El resultado de la division de "+valor1+" entre "+valor2+" es "+resultado);
 }
